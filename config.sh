429ade476e51452e478c70332f70cc423f1e9383
xDraiker <daniel.vz.gc97@gmail.com>
2022-10-03 07:40:13 -0500

    añadiendo dev server

--- config.sh
+++ config.sh
@@ -3,5 +3,5 @@
 # para ejecutar ./config.sh 
 
 npm init -y
-sed -i 's/"scripts": {/"scripts": {\n\t"dev": "rm -rf dist \&\& webpack \-\-mode development",\n\t"build": "rm -rf dist \&\& webpack \-\-mode production",/g' package.json
-npm i -D webpac webpack-cli babel-loader @babel/core @babel/preset-env html-webpack-plugin mini-css-extract-plugin css-loader postcss-loader postcss-preset-env cssnano postcss
+sed -i 's/"scripts": {/"scripts": {\n\t"dev": "rm -rf dist \&\& webpack \-\-mode development",\n\t"build": "rm -rf dist \&\& webpack \-\-mode production",\n\t"start": "webpack serve \-\-mode development \-\-open",/g' package.json
+npm i -D webpac webpack-cli babel-loader @babel/core @babel/preset-env html-webpack-plugin mini-css-extract-plugin css-loader postcss-loader postcss-preset-env cssnano postcss webpack-dev-server
